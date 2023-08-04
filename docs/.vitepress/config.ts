f387239de8d5c3017e68a65e5907c63479b78f30
19857191790 <>
2023-08-04 11:56:11 +0800

    ~

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from "vitepress";
+
+const sidebar = {
+    '/experience/': [
+        {
+            text: 'Experience',
+            items: [
+                { text: 'One', link: '/experience/' },
+            ]
+        }
+    ]
+}
+
+
+export default defineConfig({
+    title: '某公司摸鱼前端的Blog',
+    description: '某公司摸鱼前端的Blog',
+    themeConfig:{
+        nav:[
+            {text:'Home',link:'/'},
+            {text:'Experience',link:'/experience/'},
+            {text:'About',link:'/about'},
+            {text:'Gitee',link:'https://gitee.com/zhaojc-077399'},
+            {text:'GitHub',link:'https://github.com/19857191790/blog'},
+            {text:'CSDN',link:'https://blog.csdn.net/weixin_59685936?type=blog'},
+        ],
+        sidebar:sidebar
+    }
+})
---
+++ docs/index.md
@@ -0,0 +1,30 @@
+---
+layout: home
+hero:
+  name: 我将在这里分享我的经验
+  text: 某公司摸鱼前端
+  tagline: 摸鱼是一种生活态度
+  actions: 
+    - text: Look My Experience
+      link: /experience/
+  image:
+    alt: 某公司摸鱼前端
+    src: /images/hero-img.jpg
+
+features:
+  - title: MobilePhone
+    details: "19857191790"
+  - title: WeChat
+    details: "1985791790"
+  - title: WeChatMiniProgram
+    details: 正在尝试插入图片
+---
+
+<style>
+  .VPHero .container .image .image-container{
+    transform: unset;
+  }
+  .VPHero .container .image .image-container img{
+    height:100%;
+  }
+</style>
